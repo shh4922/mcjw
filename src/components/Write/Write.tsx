0f8391e7cba583375437103d5aa78581ade5fb55
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2024-10-19 16:18:36 +0900

    ad

--- src/components/Write/Write.tsx
+++ src/components/Write/Write.tsx
@@ -5,13 +5,15 @@ import { useNavigate } from "react-router-dom";
 import ReactTextareaAutosize from "react-textarea-autosize";
 import { useAuth } from "../../context/AuthContext";
 import "./write.css"
+import { error } from "console";
+
 
 function Write() {
     const { currentUser } = useAuth();
     const [title, setTitle] = useState("")
     const [content, setContent] = useState("")
     const navigator = useNavigate()
-    
+
     const handleTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
         const copyTitle = e.target.value
         setTitle(copyTitle)
@@ -20,45 +22,41 @@ function Write() {
         const copyContent = e.target.value
         setContent(copyContent)
     }
-
-    const handlePost =  () => {
-        if(title === "" || content === "") {
-            alert("제목과 내용을 입력해주세요")
-            return
-        }   
-        writePost()
-    }
-
-    const writePost = async () => {
+    const writePost = async (body: Object) => {
         try {
-            const body = {
-                title : title,
-                content : content
+            const reuslt = customAxios.post(`${API.BASE_URL}${API.WRITE_POST}`, body)
+            if ((await reuslt).status === 200) {
+                alert("게시물에 등록되었습니다.")
+                navigator("/my")
+            } else {
+                alert("게시물등록에 실패했습니다.")
+                navigator("/")
             }
-            const reuslt = await customAxios.post(`${API.BASE_URL}${API.WRITE_POST}`, body)
-            
-            alert("게시물에 등록되었습니다.")
-            navigator("/my")
-        } catch(error) {
-            console.error(error)           
+
+        } catch (error) {
+            if (error === "402") {
+                alert("세션이만료되어")
+                navigator("/login")
+                return
+            }
+
             navigator("/")
         }
     }
-
-    // 글작성 은 크루권한만 가능.
-    useEffect(() => {
-        if(currentUser?.auth !== "crew") {
-            alert("권한이없습니다")
-            navigator("/")
-            return
+    const handlePost = () => {
+        const body = {
+            title: title,
+            content: content
         }
-    },[])
+
+        writePost(body)
+    }
 
     return (
         <div className="write">
             <div className="write-wrap">
-                <input className = "title" defaultValue={title} placeholder="title" onChange={handleTitle}></input>
-                <ReactTextareaAutosize className = "content" defaultValue={content} placeholder="content" onChange={handleContent}></ReactTextareaAutosize>
+                <input className="title" defaultValue={title} placeholder="title" onChange={handleTitle}></input>
+                <ReactTextareaAutosize className="content" defaultValue={content} placeholder="content" onChange={handleContent}></ReactTextareaAutosize>
                 <button onClick={handlePost}>게시</button>
             </div>
         </div>
